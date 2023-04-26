1943a12beeb845d585967d48a8d36c31ada01a05
Rahul Kumar Verma <rahul.verma@brainerhub.com>
2023-04-26 18:27:57 +0530

    Add : Intial Admin portel and slde bar

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Switch } from "react-router-dom";
+import { AppUrls } from "./AppUrl";
+
+import Home from "./pages/private-pages/home/Home";
+import Panels from "./pages/private-pages/panels/Panels";
+import Projects from "./pages/private-pages/Projects/Projects";
+import Scheduler from "./pages/private-pages/scheduler/Scheduler";
+import Surveys from "./pages/private-pages/surveys/Surveys";
+import SignIn from "./pages/public-pages/sign-in/SignIn";
+
+import PrivateRoute from "./routes/privateRoute";
+import PublicRoute from "./routes/publicRoute";
+
+const AppRoutes = () => {
+  return (
+    <Switch>
+      <PublicRoute path={AppUrls.PublicUrl.SignUp}>
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path={AppUrls.Client.Panels}>
+        <Panels />
+      </PrivateRoute>
+      <PrivateRoute path={AppUrls.Client.Schedules}>
+        <Scheduler />
+      </PrivateRoute>
+      <PrivateRoute path={AppUrls.Client.Surveys}>
+        <Surveys />
+      </PrivateRoute>
+      <PrivateRoute path={AppUrls.Client.Project}>
+        <Projects />
+      </PrivateRoute>
+      <PrivateRoute path={AppUrls.Client.Home}>
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
+};
+
+export default AppRoutes;
---
+++ src/routes/publicRoute.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Redirect, RouteProps } from "react-router-dom";
+
+const PublicRoute = ({ children, ...rest }: RouteProps) => {
+  const isAuthenticated = localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    <Redirect to="/home" />
+  ) : (
+    <Route {...rest}>{children}</Route>
+  );
+};
+
+export default PublicRoute;
+
+
+// Notes security stuff we can manage here as well cache & cookies with dynamic data  
